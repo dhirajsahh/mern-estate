6391ad5614e881c9640aeea642e757ac77d2a8aa
dhirajsahh <dhirajmahato2019@gmail.com>
2023-12-10 22:03:58 +0545

    create page and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,23 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-up" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
